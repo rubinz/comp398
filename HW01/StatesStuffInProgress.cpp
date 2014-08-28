f9800ca4d32a97dc1c654f4ccc69d4baa424057c
rubinz <zevirc@gmail.com>
2014-08-28 14:24:16 -0400

    adding states file

---
+++ HW01/StatesStuffInProgress.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+struct NODE
+{
+    string data;
+    NODE *next;
+};
+
+int main()
+{
+    ifstream myfile ("States.csv");
+
+    NODE *start = NULL,*ptr,*temp;
+    string line;
+
+    if (myfile.is_open()){
+
+        while (getline(myfile,line))
+
+        {
+            cout << line << endl;
+            ptr = new NODE;
+            ptr->data=line;
+            ptr->next=NULL;
+            if(start==NULL)
+                start=ptr;
+            else
+            {
+                temp=start;
+                while(temp->next!=NULL)
+                    temp=temp->next;
+                temp->next=ptr;
+            }
+        }
+    }
+    myfile.close();
+    temp=start;
+    while(temp->next!=NULL)
+    {
+        cout<<temp->data<<"  ";
+        temp=temp->next;
+    }
+    return 0;
+}
